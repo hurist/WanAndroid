91ed1fd674f8b06a149d791174be8a42bf0e40e7
hu jie <hujie@jobwen.com>
2020-05-20 13:36:31 +0800

    first commit

---
+++ app/src/main/java/com/hurist/wanandroid/MainViewModel.kt
@@ -0,0 +1,15 @@
+package com.hurist.wanandroid
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class MainViewModel(): ViewModel() {
+
+
+    fun test() {
+        viewModelScope.launch {
+
+        }
+    }
+}
